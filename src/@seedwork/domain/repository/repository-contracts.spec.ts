43c4c66e64ad30d31b483523541e562cf897da6c
Willians <williansvf@hotmail.com>
2023-08-05 16:59:40 -0300

    feat: busca personalizada

--- src/@seedwork/domain/repository/repository-contracts.spec.ts
+++ src/@seedwork/domain/repository/repository-contracts.spec.ts
@@ -1,4 +1,4 @@
-import { SearchParams } from "./repository-contracts"
+import { SearchParams, SearchResult } from "./repository-contracts"
 
 describe('SearchParams Unit Tests', () => {
     test('page prop', () => {
@@ -79,7 +79,7 @@ describe('SearchParams Unit Tests', () => {
         let params = new SearchParams();
         expect(params.sort_dir).toBeNull()
 
-        params = new SearchParams({sort:null});
+        params = new SearchParams({ sort: null });
         expect(params.sort_dir).toBeNull()
 
         const arrange: { sort_dir: any, expect: any }[] = [
@@ -92,11 +92,11 @@ describe('SearchParams Unit Tests', () => {
             { sort_dir: "ASC", expect: "asc" },
             { sort_dir: "desc", expect: "desc" },
             { sort_dir: "DESC", expect: "desc" },
-            
+
         ]
 
         arrange.forEach(i => {
-            expect(new SearchParams({ sort: "field", sort_dir:i.sort_dir }).sort_dir).toBe(i.expect)
+            expect(new SearchParams({ sort: "field", sort_dir: i.sort_dir }).sort_dir).toBe(i.expect)
         })
     })
 
@@ -123,5 +123,61 @@ describe('SearchParams Unit Tests', () => {
         arrange.forEach(i => {
             expect(new SearchParams({ filter: i.filter }).filter).toBe(i.expect)
         })
+    })
+})
+
+describe('SearchResult Unit Tests', () => {
+    test('', () => {
+        const result = new SearchResult({
+            items: ["entity1", "entity2"] as any,
+            total: 4,
+            current_page: 1,
+            per_page: 2,
+            sort: null,
+            sort_dir: null,
+            filter: null
+        }
+        )
+
+        expect(result.toJSON()).toStrictEqual({
+            items: ["entity1", "entity2"] as any,
+            total: 4,
+            current_page: 1,
+            per_page: 2,
+            last_page: 2,
+            sort: null,
+            sort_dir: null,
+            filter: null
+        })
+    })
+
+    it('should set last_page 1 when per_page field is greater tham total field',()=>{
+        const result = new SearchResult({
+            items: ["entity1", "entity2"] as any,
+            total: 4,
+            current_page: 1,
+            per_page: 15,
+            sort: "name",
+            sort_dir: "asc",
+            filter: "test"
+        }
+        )
+
+        expect(result.last_page).toBe(1)
+    })
+
+    it('when total is not a multiple of per_page',()=>{
+        const result = new SearchResult({
+            items: [] as any,
+            total: 101,
+            current_page: 1,
+            per_page: 20,
+            sort: "name",
+            sort_dir: "asc",
+            filter: "test"
+        }
+        )
+
+        expect(result.last_page).toBe(6)
     })
 })
--- src/@seedwork/domain/repository/repository-contracts.ts
+++ src/@seedwork/domain/repository/repository-contracts.ts
@@ -54,7 +54,7 @@ export class SearchParams {
     }
 
     private set per_page(value: number) {
-        let _per_page = value === true as any? this._per_page: +value
+        let _per_page = value === true as any ? this._per_page : +value
 
         if (Number.isNaN(_per_page) || _per_page <= 0 || parseInt(_per_page as any) !== _per_page) {
             _per_page = 15
@@ -96,10 +96,57 @@ export class SearchParams {
     }
 }
 
+type SearResultProps<E extends Entity, Filter = string> = {
+    readonly items: E[];
+    readonly total: number;
+    readonly current_page: number;
+    readonly per_page: number;
+    readonly sort?: string | null;
+    readonly sort_dir: string | null;
+    readonly filter: Filter;
+}
+
+export class SearchResult<E extends Entity, Filter = string> {
+
+    readonly items: E[];
+    readonly total: number;
+    readonly current_page: number;
+    readonly last_page: number;
+    readonly per_page: number;
+    readonly sort?: string | null;
+    readonly sort_dir: string | null;
+    readonly filter: Filter;
+
+    constructor(props: SearResultProps<E, Filter>) {
+        this.items = props.items;
+        this.total = props.total;
+        this.current_page = props.current_page;
+        this.per_page = props.per_page;
+        this.last_page = Math.ceil(this.total / this.per_page)
+        this.sort = props.sort ?? null;
+        this.sort_dir = props.sort_dir ?? null;
+        this.filter = props.filter ?? null;
+    }
+
+    toJSON() {
+        return {
+            items: this.items,
+            total: this.total,
+            current_page: this.current_page,
+            per_page: this.per_page,
+            last_page: this.last_page,
+            sort: this.sort,
+            sort_dir: this.sort_dir,
+            filter: this.filter,
+        }
+    }
+}
+
 export interface SearchbleRepositoryInterface<
     E extends Entity,
-    SearchInput,
-    SearchOutPut>
+    Filter = string,
+    SearchInput = SearchParams,
+    SearchOutPut = SearchResult<E,Filter>>
     extends RepositoryInterface<E> {
     search(props: SearchInput): Promise<SearchOutPut>;
 }
